dde5a757aa3124ebd14021b0c743cddb6460d9a2
Ken Hoff <ken@hoff.tech>
2018-01-27 11:28:23 -0700

    starting to add different responses from different people in the dialog box

--- src/start.jsx
+++ src/start.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import "../styles/main.scss";
-
-let textOptions = ["Open the pod bay doors, hal.", "I'm afraid i can't do that, dave.", "daisy, daisy",]
+import '../styles/main.scss';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputText: "",
-            textBits: [],
-        }
+            inputText: '',
+            textBits: []
+        };
     }
     render() {
         return (<div>
@@ -21,20 +19,57 @@ class App extends React.Component {
                     this
                         .state
                         .textBits
-                        .map((textBit) => {
-                            return (<div key={Math.random()}>{textBit}</div>)
+                        .map(textBit => {
+                            return (<div key={Math.random()} className={`${textBit.character}-response`}>
+                                {textBit.text}
+                            </div>);
                         })
                 }
-            </div>
-            <div>
-                <button onClick={() => {
+                <button className="player-option" onClick={() => {
                         let newTextBits = [
-                            ...this.state.textBits,
-                            textOptions[Math.floor(Math.random() * textOptions.length)],
+                            ...this.state.textBits, {
+                                character: 'player',
+                                text: 'Hello, HAL. Do you read me, HAL?'
+                            }, {
+                                character: 'entity',
+                                text: 'Affirmative, Dave. I read you.'
+                            },
                         ];
-                        this.setState({textBits: newTextBits, inputText: "",});
-
+                        this.setState({textBits: newTextBits});
                     }}>
-                    add new text bit ></button>
+                    Hello, HAL. Do you read me, HAL?
+                </button>
+                <button className="player-option" onClick={() => {
+                        let newTextBits = [
+                            ...this.state.textBits, {
+                                character: 'player',
+                                text: 'Open the pod bay doors, HAL.'
+                            }, {
+                                character: 'entity',
+                                text: "I'm sorry, Dave. I'm afraid I can't do that."
+                            },
+                        ];
+                        this.setState({textBits: newTextBits});
+                    }}>
+                    Open the pod bay doors, HAL.
+                </button>
+                <button className="player-option" onClick={() => {
+                        let newTextBits = [
+                            ...this.state.textBits, {
+                                character: 'player',
+                                text: 'What are you talking about, HAL?'
+                            }, {
+                                character: 'entity',
+                                text: 'This mission is too important for me to allow you to jeopardize it.'
+                            },
+                        ];
+                        this.setState({textBits: newTextBits});
+                    }}>
+                    What are you talking about, HAL?
+                </button>
             </div>
-        </div>); } } export default App; ReactDOM.render(<App/>, document.getElementById('app'));
+        </div>);
+    }
+}
+export default App;
+ReactDOM.render(<App/>, document.getElementById('app'));
